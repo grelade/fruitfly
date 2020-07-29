fd797c390d742477ad6c3080db468a64cc9f3db8
grelade <jacekgrela@gmail.com>
2020-07-29 14:36:04 +0200

    bugfix

--- download_primary_roi_datasets.py
+++ download_primary_roi_datasets.py
@@ -1,5 +1,8 @@
+from neuprint import Client
+
 from config import conf
 from dataset_utils import fetch_primary_roi_datasets
+
 
 c = Client(conf.neuprint_URL, conf.dataset_version,conf.api_token)
 er = fetch_primary_roi_datasets(client=c)
